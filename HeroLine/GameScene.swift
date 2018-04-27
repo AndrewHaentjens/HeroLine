86c9be2071ab80a2cbe0052176397802d8bf1bc6
Andrew Haentjens <andrew@nextapps.be>
2018-04-27 13:40:53 +0200

    Initial commit

---
+++ HeroLine/GameScene.swift
@@ -0,0 +1,25 @@
+//
+//  GameScene.swift
+//  HeroLine
+//
+//  Created by Andrew Haentjens on 27/04/2018.
+//  Copyright © 2018 Andrew Haentjens. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var entities = [GKEntity]()
+    var graphs = [String : GKGraph]()
+    
+    private var lastUpdateTime : TimeInterval = 0
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func sceneDidLoad() {
+
+        
+    }
+}
---
+++ HeroLine/GameViewController.swift
@@ -0,0 +1,40 @@
+//
+//  GameViewController.swift
+//  HeroLine
+//
+//  Created by Andrew Haentjens on 27/04/2018.
+//  Copyright © 2018 Andrew Haentjens. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.frame.size)
+        scene.scaleMode = .aspectFill
+        
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        
+        skView.presentScene(scene)
+        
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscape
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
